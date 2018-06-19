9c70904aa08e44267035163a436df1149d5bbed9
Kathryn Parker <kparker@warpedworks.com>
2018-06-19 19:33:49 -0400

    class notes

--- public/exercise1.js
+++ public/exercise1.js
@@ -1,12 +1,15 @@
 // EXERCISE 1
 // This counter isn't working! The counter always says 0 even when you press 'increment'
-// This is because the reducer isn't implemented 
+// This is because the reducer isn't implemented
 
 let reducer1 = (state = 0, action) => {
 	// Write code here that will:
 	// 1) check to see if the action is of type "INCREMENT" (see the dispatch call below)
+		if(action.type== "INCREMENT") {}
 	// 2) return state + 1 if it's and "INCREMENT"
+		return state +1
 	// 3) otherwise just return state
+
 	// (HINT - look at the other exercises to see what might go here)
 	return state;
 }
--- public/exercise3.js
+++ public/exercise3.js
@@ -1,6 +1,6 @@
 // EXERCISE 3
 // Let's make the checkbox label say 'checked' whenever we check the checkbox
-// The subscribe() callback is unimplemented -> 
+// The subscribe() callback is unimplemented ->
 
 let defaultState = {
 	isChecked: false
@@ -8,7 +8,7 @@ let defaultState = {
 
 let reducer3 = (state = defaultState, action) => {
 
-	var stateCopy = Object.assign({}, state);// This is a very ES5 way to copy an object 
+	var stateCopy = Object.assign({}, state);// This is a very ES5 way to copy an object
 
 	if (action.type == "CLICK_CHECKBOX") {
 		stateCopy.isChecked = !stateCopy.isChecked;
@@ -25,10 +25,20 @@ let checkboxLabelHTML = document.getElementById('checkbox-label');
 
 
 // use store3.subscribe() to register a callback
-// use store3.getState() in the callback to read the current state 
+// use store3.getState() in the callback to read the current state
 // use the current state to decide whether checkboxLabelHTML needs to be 'checked' or 'unchecked'
 // use checkboxLabelHTML.innerHTML == ...   to update the label with the correct word
+store3.subscribe(()=>{
+	let state = store3.getState();
 
+	if (state.isChecked) {
+		// manipulate the DOM to make the label say 'checked'
+		checkboxLabelHTML.innerHTML = 'checked';
+	} else {
+		// // manipulate the DOM to make the label say 'unchecked'
+		checkboxLabelHTML.innerHTML = 'unchecked';
+	}
+});
 
 checkboxHTML.addEventListener('change', (e)=>{
 	store3.dispatch({
